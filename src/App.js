4ca4a1a0188dc5dbad2c5a2d9b8620d8361d9b2f
Alek <alekhawkins17@gmail.com>
2020-06-11 15:52:57 -0700

    added animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import dotAnimation from './dotAnimation/dotAnimation';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <dotAnimation />
       <Footer />
     </div>
   );
